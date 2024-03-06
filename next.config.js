5dd747f4165754dd5e52dad005c02b8c43b908f6
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2024-03-06 01:11:02 +0000

    Update images location
    Signed-off-by: miam-miam100 <49870539+miam-miam100@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
+    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL, 'https://society.ecs.soton.ac.uk']
       .filter(Boolean)
       .map(url => url.replace(/https?:\/\//, '')),
   },
