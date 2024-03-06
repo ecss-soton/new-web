9e14828d0a3add0e24ee762f6112ebaffafdff0c
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2024-03-06 12:14:39 +0000

    Update favicon to ECSS logo and remove some mentions to payload template.
    Signed-off-by: miam-miam100 <49870539+miam-miam100@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
-        <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Providers>
