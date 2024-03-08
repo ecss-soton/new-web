be36640d48d0afcb6ab5149aa09e4a54a3d2a460
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-08 20:10:07 +0000

    Use doc over data

--- src/payload/collections/Nominations/access/beforeVoting.ts
+++ src/payload/collections/Nominations/access/beforeVoting.ts
@@ -3,12 +3,12 @@ import type { FieldAccess } from 'payload/types'
 import type { Election, Nomination } from '../../../payload-types'
 import { checkRole } from '../../Users/checkRole'
 
-export const beforeVoting: FieldAccess<Nomination> = ({ req: { user }, data }) => {
+export const beforeVoting: FieldAccess<Nomination> = ({ req: { user }, doc }) => {
   if (user && checkRole(['admin'], user)) {
     return true
   }
 
-  const election = data.election as Election
+  const election = doc.election as Election
   const votingStart = Date.parse(election.votingStart)
   const now = new Date().getTime()
 
