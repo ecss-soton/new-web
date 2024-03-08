ba09a3eeabf6e368b9d6cdec82abbefd122db88f
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2024-03-08 20:49:04 +0000

    Fix beforeVoting access check

--- src/payload/collections/Nominations/access/beforeVoting.ts
+++ src/payload/collections/Nominations/access/beforeVoting.ts
@@ -3,13 +3,21 @@ import type { FieldAccess } from 'payload/types'
 import type { Election, Nomination } from '../../../payload-types'
 import { checkRole } from '../../Users/checkRole'
 
-export const beforeVoting: FieldAccess<Nomination> = ({ req: { user }, doc }) => {
-  if (user && checkRole(['admin'], user)) {
+export const beforeVoting: FieldAccess<Nomination> = async ({ req, doc, data }) => {
+  if (req.user && checkRole(['admin'], req.user)) {
     return true
   }
 
-  const election = doc.election as Election
-  const votingStart = Date.parse(election.votingStart)
+  // Don't think doc.election will ever not be a string, but just in case
+  const election = typeof doc.election === 'string' ? doc.election : doc.election.id
+
+  const result = await req.payload.findByID({
+    collection: 'elections',
+    id: election,
+    depth: 0,
+  })
+
+  const votingStart = Date.parse(result.votingStart)
   const now = new Date().getTime()
 
   return now < votingStart
