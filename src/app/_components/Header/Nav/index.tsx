c7cc7e2428bf3a3c30f8ab8ef06384b86d0b69a7
Casper <148681165+casperUoS@users.noreply.github.com>
2024-09-06 13:25:32 +0100

    New header

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -1,17 +1,26 @@
 'use client'
 
 import React from 'react'
+import { Inter } from '@next/font/google'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
-import { Header as HeaderType } from '../../../../payload/payload-types'
+import { Header as HeaderType, Page } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
 import { CMSLink } from '../../Link'
 
 import classes from './index.module.scss'
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal'],
+})
+
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const navItems = header?.navItems || []
   const { user } = useAuth()
+  const currentPath = usePathname()
 
   return (
     <nav
@@ -25,9 +34,28 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
         .join(' ')}
     >
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="none" />
+        const slug = (link.reference?.value as Page)?.slug
+        const isActive = link.url === currentPath || `/${slug}` === currentPath
+        const label = isActive ? (
+          <>
+            <span className={classes.redBrackets}>[&nbsp;</span>
+            {link.label}
+            <span className={classes.redBrackets}>&nbsp;]</span>
+          </>
+        ) : (
+          <>
+            <span className={classes.redBrackets}>&nbsp;&nbsp;</span>
+            {link.label}
+            <span className={classes.redBrackets}>&nbsp;&nbsp;</span>
+          </>
+        )
+        return <CMSLink key={i} {...link} label={label} appearance="header" />
       })}
-      {user && <Link href="/account">Account</Link>}
+      {user && (
+        <Link href="/account" className={inter.className}>
+          Account
+        </Link>
+      )}
       {/*
         // Uncomment this code if you want to add a login link to the header
         {!user && (
