fb414b120329adcd6544ce4697381002a311e419
Casper <148681165+casperUoS@users.noreply.github.com>
2025-03-06 15:58:38 +0000

    fixed committee counter

--- src/app/_blocks/HomeTop/index.tsx
+++ src/app/_blocks/HomeTop/index.tsx
@@ -1,7 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import { Inter } from '@next/font/google'
+'use client'
 
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useRef, useState } from 'react'
+import { Inter } from '@next/font/google'
+import qs from 'qs'
+
+import type { Committee, Sponsor } from '../../../payload/payload-types'
 import { Page } from '../../../payload/payload-types'
 import { Button } from '../../_components/Button'
 import { Gutter } from '../../_components/Gutter'
@@ -17,6 +21,10 @@ const inter = Inter({
   style: ['normal'],
 })
 
+type Result = {
+  docs: (Sponsor | string)[]
+}
+
 type Props = Extract<Page['layout'][0], { blockType: 'homeTop' }>
 
 export const HomeTopBlock: React.FC<
@@ -24,6 +32,82 @@ export const HomeTopBlock: React.FC<
     id?: string
   }
 > = ({ heading, image1, image2, image3 }) => {
+  const [results, setResults] = useState<Result>({
+    docs: []?.map(doc => doc.value),
+    // hasNextPage: false,
+    // hasPrevPage: false,
+    // nextPage: 1,
+    // page: 1,
+    // prevPage: 1,
+    // totalDocs: typeof populatedDocsTotal === 'number' ? populatedDocsTotal : 0,
+    // totalPages: 1,
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<string | undefined>(undefined)
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const hasHydrated = useRef(false)
+  const isRequesting = useRef(false)
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout = null
+
+    if (!isRequesting.current) {
+      isRequesting.current = true
+
+      // hydrate the block with fresh content after first render
+      // don't show loader unless the request takes longer than x ms
+      // and don't show it during initial hydration
+      timer = setTimeout(() => {
+        if (hasHydrated.current) {
+          setIsLoading(true)
+        }
+      }, 500)
+
+      const searchQuery = qs.stringify(
+        {
+          depth: 1,
+          limit: 20,
+        },
+        { encode: false },
+      )
+
+      const makeRequest = async () => {
+        try {
+          const req = await fetch(
+            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/committee?${searchQuery}`,
+          )
+
+          const json = await req.json()
+          clearTimeout(timer)
+
+          const { docs } = json as { docs: Committee[] }
+
+          if (docs && Array.isArray(docs)) {
+            setResults(json)
+            setIsLoading(false)
+            // if (typeof onResultChange === 'function') {
+            //   onResultChange(json)
+            // }
+          }
+        } catch (err) {
+          console.warn(err) // eslint-disable-line no-console
+          setIsLoading(false)
+          setError(`Unable to load "sponsor archive" data at this time.`)
+        }
+
+        isRequesting.current = false
+        hasHydrated.current = true
+      }
+
+      void makeRequest()
+    }
+
+    return () => {
+      if (timer) clearTimeout(timer)
+    }
+  }, [])
+
   return (
     <VerticalPadding top="none" bottom="none">
       <div className={[classes.container, inter.className].join(' ')}>
@@ -41,7 +125,7 @@ export const HomeTopBlock: React.FC<
               <span className={classes.label}>members</span>
             </div>
             <div className={classes.stat}>
-              <span className={classes.number}>12</span>
+              <span className={classes.number}>{results.docs?.length}</span>
               <span className={classes.label}>committee</span>
             </div>
           </div>
