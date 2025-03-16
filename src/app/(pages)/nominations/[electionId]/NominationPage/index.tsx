1e21c841b9446630d18af945147a00620ac7ce9c
Casper <148681165+casperUoS@users.noreply.github.com>
2025-03-16 00:51:18 +0000

    temp commit

--- src/app/(pages)/nominations/[electionId]/NominationPage/index.tsx
+++ src/app/(pages)/nominations/[electionId]/NominationPage/index.tsx
@@ -40,6 +40,32 @@ export const NominationPage: React.FC<{
     return <Fragment>Loading...</Fragment>
   }
 
+  let elections: (Omit<Election, 'positions'> & { positions: Position[] })[] | null = null
+
+  const searchQuery = qs.stringify(
+    {
+      depth: 2,
+      sort: '-nominationStart',
+    },
+    { encode: false },
+  )
+
+  try {
+    const req = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/elections?${searchQuery}`)
+
+    const json = await req.json()
+
+    const { docs } = json as { docs: (Election & { positions: Position[] })[] }
+
+    elections = docs
+  } catch (err) {
+    console.warn(err) // eslint-disable-line no-console
+  }
+
+  if (!elections) {
+    notFound()
+  }
+
   const names = nomination.populatedNominees.map(n => n.name).join(' & ')
   const position = nomination.position as Position
   const isMyNomination = nomination.populatedNominees.some(p => p.id === user.id)
@@ -48,12 +74,15 @@ export const NominationPage: React.FC<{
   return (
     <Fragment>
       <Gutter>
-        <Button href={`/elections`} appearance="primary" label={'Back'} />
-        <h3>
-          {droppedOut && <s>{nomination.nickname ?? names}</s>}
-          {!droppedOut && <span>{nomination.nickname ?? names}</span>}
-        </h3>
-        {/* {nomination.populatedNominees.map(n => {
+        {isMyNomination ? (
+          <>
+            <Button href={`/elections`} appearance="primary" label={'Back'} />
+            <h3>
+              {droppedOut && <s>{nomination.nickname ?? names}</s>}
+              {!droppedOut && <span>{nomination.nickname ?? names}</span>}
+            </h3>
+
+            {/* {nomination.populatedNominees.map(n => {
           const email = `${n.username}@soton.ac.uk`
           return (
             <Fragment key={n.id}>
@@ -62,18 +91,25 @@ export const NominationPage: React.FC<{
             </Fragment>
           )
         })} */}
-        <h4>Running for {position.name}</h4>
-        {isMyNomination && (
-          <Button
-            href={`/nominations/${nominationId}/edit`}
-            appearance="primary"
-            label={'Edit Nomination'}
-          />
+            <h4>Running for {position.name}</h4>
+            {isMyNomination && (
+              <Button
+                href={`/nominations/${nominationId}/edit`}
+                appearance="primary"
+                label={'Edit Nomination'}
+              />
+            )}
+            <Suspense fallback={<Fragment>Loading...</Fragment>}>
+              <Media resource={nomination.image} imgClassName={classes.image} />
+            </Suspense>
+            <span className={classes.manifesto}>{nomination.manifesto}</span>
+          </>
+        ) : (
+          <span> Error: This user does not have acess to this nomination page</span>
         )}
-        <Suspense fallback={<Fragment>Loading...</Fragment>}>
-          <Media resource={nomination.image} imgClassName={classes.image} />
-        </Suspense>
-        <span className={classes.manifesto}>{nomination.manifesto}</span>
+        {/* <span>{nomination.id} hello</span>
+        {user && <span> {user.id}</span>}
+        {!user && <span>wompwomp</span>} */}
       </Gutter>
     </Fragment>
   )
