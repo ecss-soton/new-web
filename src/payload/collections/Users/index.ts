f4df60a5dc3f02a960cf8c66d4360dfdf556cfec
Casper <148681165+casperUoS@users.noreply.github.com>
2026-03-21 19:28:15 +0000

    temp remove of susu check after creations due to bugs

--- src/payload/collections/Users/index.ts
+++ src/payload/collections/Users/index.ts
@@ -24,7 +24,7 @@ const Users: CollectionConfig = {
     admin: ({ req: { user } }) => checkRole(['admin'], user),
   },
   hooks: {
-    afterChange: [loginAfterCreate, checkSusuRoleAfterCreate],
+    afterChange: [loginAfterCreate],
   },
   auth: {
     disableLocalStrategy: true,
