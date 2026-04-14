f5bcddbae7f1cde81749734ccfdc925f153f5164
Indigo Nolan <dev@indigonolan.com>
2026-04-14 19:43:05 +0100

    add wordle game

---
+++ src/app/(pages)/wordle/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Metadata } from 'next'
+import { Gutter } from '../../_components/Gutter'
+import { LowImpactHero } from '../../_heros/LowImpact'
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
+import { WordleGame } from './WordleGame'
+
+export default async function WordlePage() {
+  return (
+    <>
+      <LowImpactHero title="Wordle" type="lowImpact" />
+      <Gutter>
+        <WordleGame />
+      </Gutter>
+    </>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Wordle',
+  description: 'Play our custom Wordle game to unlock merch!',
+  openGraph: mergeOpenGraph({
+    title: 'Wordle',
+    url: '/wordle',
+  }),
+}
