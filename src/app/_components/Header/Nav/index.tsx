e4f202f2c28d13b842ee4b15c8693a479c3867b3
Indigo Nolan <dev@indigonolan.com>
2026-04-28 12:03:36 +0100

    remove old fonts, replace

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
